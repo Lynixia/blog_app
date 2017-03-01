dbb2ba529bea0fa7ae2b5f531a72780f9851952e
Jillianne13 <jillianne.presley.1641@mail.linnbenton.edu>
2017-03-01 21:02:30 +0000

    Added About Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+    #auto generated Home page.
   get 'static_pages/home'
-
+  
+#auto generated help page.
   get 'static_pages/help'
+  
+  #auto generated About Page
+  get 'static_pages/about'
 
     root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
