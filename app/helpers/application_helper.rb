dc69d2156a406a9030a201775db5bd947c2f4365
Jillianne13 <jillianne.presley.1641@mail.linnbenton.edu>
2017-03-03 00:03:08 +0000

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    # Return the full title on a per page basis
+    #Ruby on Rails Sample App! 
+    def full_title(page_title = '')                                         
+        base_title = "Firecaller Blogs"
+        if page_title.empty? 
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end 
 end
