d7c654d804227ed5757117bc5627b014eab9f040
Jillianne13 <jillianne.presley.1641@mail.linnbenton.edu>
2017-03-19 23:36:52 +0000

    Implement Advanced Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,26 @@ class User < ApplicationRecord
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+   # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+     return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+   # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
+   class << self
+  # Returns the hash digest of the given string.
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
+   # Returns a random token.
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+ end
